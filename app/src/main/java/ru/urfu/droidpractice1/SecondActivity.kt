52375c173405f1634bd717092260326620391014
MatafonovDS <matafonovds@mail.skblab.ru>
2024-09-30 00:09:46 +0500

    Initial commmit

---
+++ app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
@@ -0,0 +1,19 @@
+package ru.urfu.droidpractice1
+
+import androidx.activity.ComponentActivity
+import android.os.Bundle
+import ru.urfu.droidpractice1.databinding.ActivitySecondBinding
+
+class SecondActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+    }
+}
---
+++ app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
@@ -0,0 +1,50 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package ru.urfu.droidpractice1.content
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import ru.urfu.droidpractice1.R
+import ru.urfu.droidpractice1.ui.theme.DroidPractice1Theme
+
+@Composable
+fun MainActivityScreen() {
+    DroidPractice1Theme {
+        Scaffold(modifier = Modifier.fillMaxSize(),
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(
+                            text = stringResource(id = R.string.article_title)
+                        )
+                    }
+                )
+            }) { innerPadding ->
+            Box(
+                modifier = Modifier.padding(innerPadding)
+            ) {
+
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainActivityScreen()
+}
